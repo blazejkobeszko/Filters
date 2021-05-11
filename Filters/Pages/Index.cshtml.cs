48d25cdf2df094a63f3e411536e57c97821b960f
Błażej <blazejkob1342@gmail.com>
2021-05-11 16:12:09 +0200

    Dodaj pliki projektów.

---
+++ Filters/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Filters.Utils;
+
+namespace Filters.Pages
+{
+    [CustomPageFilter]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ Filters/Utils/CustomPageFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Filters.Utils
+{
+
+    public class CustomPageFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["filterMessage"] = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            await next.Invoke();
+        }
+    }
+}
